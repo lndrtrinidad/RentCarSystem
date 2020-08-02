543ecdcada4021f9f9b20a78a1e6588415519503
LndrT <LndrT@LAPTOP-3U14ODTN>
2020-08-02 13:34:30 -0400

    Programacion de botones para los registros clientes y usuario

--- UI/Registros/rClientes.xaml.cs
+++ UI/Registros/rClientes.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RentCarSystem.BLL;
+using RentCarSystem.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -18,6 +20,7 @@ namespace RentCarSystem.UI.Registros
     /// </summary>
     public partial class rClientes : Window
     {
+        private Clientes Clientes = new Clientes();
         public rClientes()
         {
             InitializeComponent();
@@ -26,6 +29,74 @@ namespace RentCarSystem.UI.Registros
         private void UsuarioIdTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
             e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
+        }
+
+        private void Limpiar()
+        {
+            this.Clientes = new Clientes();
+            this.DataContext = Clientes;
+        }
+
+        private bool Validar()
+        {
+            bool esValido = true;
+
+            if (UsuarioIdTextBox.Text.Length == 0)
+            {
+                esValido = false;
+                MessageBox.Show("Transaccion Fallida", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+            return esValido;
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var articulo = ClientesBLL.Buscar(Utilidades.ToInt(UsuarioIdTextBox.Text));
+
+            if (articulo != null)
+                this.Clientes = articulo;
+            else
+                this.Clientes = new Clientes();
+
+            this.DataContext = this.Clientes;
+        }
+
+        private void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Validar())
+                return;
+
+            var paso = ClientesBLL.Guardar(Clientes);
+
+            if (paso)
+            {
+                Limpiar();
+                MessageBox.Show("Guardado correctamente!", "Exito",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+                MessageBox.Show("Transaccion Fallida", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ClientesBLL.Eliminar(Utilidades.ToInt(UsuarioIdTextBox.Text)))
+            {
+                Limpiar();
+                MessageBox.Show("Registro eliminado!", "Exito",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+                MessageBox.Show("No fue posible eliminar", "Fallo",
+                   MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
--- UI/Registros/rUsuarios.xaml.cs
+++ UI/Registros/rUsuarios.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RentCarSystem.BLL;
+using RentCarSystem.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -18,6 +20,7 @@ namespace RentCarSystem.UI.Registros
     /// </summary>
     public partial class rUsuarios : Window
     {
+        private Usuarios Usuarios = new Usuarios(); 
         public rUsuarios()
         {
             InitializeComponent();
@@ -27,6 +30,74 @@ namespace RentCarSystem.UI.Registros
         {
             e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
 
+        }
+
+        private void Limpiar()
+        {
+            this.Usuarios = new Usuarios();
+            this.DataContext = Usuarios;
+        }
+
+        private bool Validar()
+        {
+            bool esValido = true;
+
+            if (UsuarioIdTextBox.Text.Length == 0)
+            {
+                esValido = false;
+                MessageBox.Show("Transaccion Fallida", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+            return esValido;
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var articulo = UsuariosBLL.Buscar(Utilidades.ToInt(UsuarioIdTextBox.Text));
+
+            if (articulo != null)
+                this.Usuarios = articulo;
+            else
+                this.Usuarios = new Usuarios();
+
+            this.DataContext = this.Usuarios;
+        }
+
+        private void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Validar())
+                return;
+
+            var paso = UsuariosBLL.Guardar(Usuarios);
+
+            if (paso)
+            {
+                Limpiar();
+                MessageBox.Show("Guardado correctamente!", "Exito",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+                MessageBox.Show("Transaccion Fallida", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (UsuariosBLL.Eliminar(Utilidades.ToInt(UsuarioIdTextBox.Text)))
+            {
+                Limpiar();
+                MessageBox.Show("Registro eliminado!", "Exito",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+                MessageBox.Show("No fue posible eliminar", "Fallo",
+                   MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
