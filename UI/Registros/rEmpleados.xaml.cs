9f33fb56ef2b856029c34dd6ae90aaf3a8e3398f
LndrT <LndrT@LAPTOP-3U14ODTN>
2020-08-03 18:03:16 -0400

    Programacion de botones en regitro empleados

--- UI/Registros/rEmpleados.xaml.cs
+++ UI/Registros/rEmpleados.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RentCarSystem.BLL;
+using RentCarSystem.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -18,6 +20,7 @@ namespace RentCarSystem.UI.Registros
     /// </summary>
     public partial class rEmpleados : Window
     {
+        private Empleados Empleados = new Empleados();
         public rEmpleados()
         {
             InitializeComponent();
@@ -26,6 +29,74 @@ namespace RentCarSystem.UI.Registros
         private void EmpleadoIdTextBoxPreviewTextInput(object sender, TextCompositionEventArgs e)
         {
             e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
+        }
+
+        private void Limpiar()
+        {
+            this.Empleados = new Empleados();
+            this.DataContext = Empleados;
+        }
+
+        private bool Validar()
+        {
+            bool esValido = true;
+
+            if (EmpleadoIdTextBox.Text.Length == 0)
+            {
+                esValido = false;
+                MessageBox.Show("Transaccion Fallida", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+            return esValido;
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var clientes = EmpleadosBLL.Buscar(Utilidades.ToInt(EmpleadoIdTextBox.Text));
+
+            if (clientes != null)
+                this.Empleados = clientes;
+            else
+                this.Empleados = new Empleados();
+
+            this.DataContext = this.Empleados;
+        }
+
+        private void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Validar())
+                return;
+
+            var paso = EmpleadosBLL.Guardar(Empleados);
+
+            if (paso)
+            {
+                Limpiar();
+                MessageBox.Show("Guardado correctamente!", "Exito",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+                MessageBox.Show("Transaccion Fallida", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (EmpleadosBLL.Eliminar(Utilidades.ToInt(Empleados.Text)))
+            {
+                Limpiar();
+                MessageBox.Show("Registro eliminado!", "Exito",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+                MessageBox.Show("No fue posible eliminar", "Fallo",
+                   MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
