e36d74be1e8c793487f6ccfe068d3f6c6ae05b35
LndrT <LndrT@LAPTOP-3U14ODTN>
2020-08-04 15:32:03 -0400

    Programacion de los botones de Registro Caracteristicas

--- UI/Registros/rCaracteristicas.xaml.cs
+++ UI/Registros/rCaracteristicas.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RentCarSystem.BLL;
+using RentCarSystem.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -17,6 +19,7 @@ namespace RentCarSystem.UI.Registros
     /// </summary>
     public partial class rCaracteristicas : Window
     {
+        private Caracteristicas Carateristicas = new Caracteristicas();
         public rCaracteristicas()
         {
             InitializeComponent();
@@ -25,6 +28,74 @@ namespace RentCarSystem.UI.Registros
         private void CaracteristicaIdTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
 
+        }
+
+        private void Limpiar()
+        {
+            this.Carateristicas = new Caracteristicas();
+            this.DataContext = Carateristicas;
+        }
+
+        private bool Validar()
+        {
+            bool esValido = true;
+
+            if (CaracteristicaIdTextBox.Text.Length == 0)
+            {
+                esValido = false;
+                MessageBox.Show("Transaccion Fallida", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+            return esValido;
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var Caracteristicas = CaracteristicasBLL.Buscar(Utilidades.ToInt(CaracteristicaIdTextBox.Text));
+
+            if (Caracteristicas != null)
+                this.Carateristicas = Caracteristicas;
+            else
+                this.Carateristicas = new Caracteristicas();
+
+            this.DataContext = this.Carateristicas;
+        }
+
+        private void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Validar())
+                return;
+
+            var paso = CaracteristicasBLL.Guardar(Carateristicas);
+
+            if (paso)
+            {
+                Limpiar();
+                MessageBox.Show("Guardado correctamente!", "Exito",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+                MessageBox.Show("Transaccion Fallida", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (CaracteristicasBLL.Eliminar(Utilidades.ToInt(CaracteristicaIdTextBox.Text)))
+            {
+                Limpiar();
+                MessageBox.Show("Registro eliminado!", "Exito",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+                MessageBox.Show("No fue posible eliminar", "Fallo",
+                   MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
