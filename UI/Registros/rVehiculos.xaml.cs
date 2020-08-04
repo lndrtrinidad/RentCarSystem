16127c0594040362797bd07d3c1e4189b47ebabc
LndrT <LndrT@LAPTOP-3U14ODTN>
2020-08-03 21:20:30 -0400

    programacion de botones de registro vehiculo

--- UI/Registros/rVehiculos.xaml.cs
+++ UI/Registros/rVehiculos.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RentCarSystem.BLL;
+using RentCarSystem.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -18,6 +20,7 @@ namespace RentCarSystem.UI.Registros
     /// </summary>
     public partial class rVehiculos : Window
     {
+        private Vehiculos Vehiculos = new Vehiculos();
         public rVehiculos()
         {
             InitializeComponent();
@@ -26,6 +29,74 @@ namespace RentCarSystem.UI.Registros
         private void VehiculoIdTextBoxPreviewTextInput(object sender, TextCompositionEventArgs e)
         {
             e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
+        }
+
+        private void Limpiar()
+        {
+            this.Vehiculos = new Vehiculos();
+            this.DataContext = Vehiculos;
+        }
+
+        private bool Validar()
+        {
+            bool esValido = true;
+
+            if (VehiculoIdTextBox.Text.Length == 0)
+            {
+                esValido = false;
+                MessageBox.Show("Transaccion Fallida", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+            return esValido;
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var vehiculos = VehiculosBLL.Buscar(Utilidades.ToInt(VehiculoIdTextBox.Text));
+
+            if (vehiculos != null)
+                this.Vehiculos = vehiculos;
+            else
+                this.Vehiculos = new Vehiculos();
+
+            this.DataContext = this.Vehiculos;
+        }
+
+        private void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Validar())
+                return;
+
+            var paso = VehiculosBLL.Guardar(Vehiculos);
+
+            if (paso)
+            {
+                Limpiar();
+                MessageBox.Show("Guardado correctamente!", "Exito",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+                MessageBox.Show("Transaccion Fallida", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (VehiculosBLL.Eliminar(Utilidades.ToInt(VehiculoIdTextBox.Text)))
+            {
+                Limpiar();
+                MessageBox.Show("Registro eliminado!", "Exito",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+                MessageBox.Show("No fue posible eliminar", "Fallo",
+                   MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
