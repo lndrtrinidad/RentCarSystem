0678aad5b25061d9ba38959748238663000bb298
Andy Stward Lanfranco Ayala <andy_lanfranco@ucne.edu.do>
2020-08-05 16:32:03 -0400

    Cambios Menores

--- BLL/UserInfo.cs
+++ BLL/UserInfo.cs
@@ -4,6 +4,7 @@ using System.Text;
 
 namespace RentCarSystem.BLL
 {
+    //Esta clase me ayuda a validar la informacion de usuario para asi no tener que poner el Login en el starupi
     public static class UserInfo
     {
         public static bool IsLoggedIn { get; set; }
