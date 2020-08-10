8efcb5eb9a3d355d3816ead2ff46420ec6c65e93
Andy Stward Lanfranco Ayala <andy_lanfranco@ucne.edu.do>
2020-08-09 22:45:46 -0400

    Se agrego el Application_DispatcherUnhandledException

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,12 @@ namespace RentCarSystem
     /// </summary>
     public partial class App : Application
     {
+        
+            private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+            {
+                MessageBox.Show($"Ha ocurrido un error no controlado:\n\n {e.Exception.Message}");
+                e.Handled = true;
+            }
+        
     }
 }
